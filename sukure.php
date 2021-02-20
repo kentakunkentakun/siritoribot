2424d389d6c3181a025ed8ea6cf2b2498c85198f
kentakunkentakun <kenta.0602.2000@icloud.com>
2021-02-21 04:41:01 +0900

    jj

--- sukure.php
+++ sukure.php
@@ -5,6 +5,6 @@
         $html = file_get_contents("https://www.weblio.jp/content/" . $text);
         $contents = phpQuery::newDocument($html)->find('.kijiWrp')->find(".midashigo");
         foreach($contents as $content){
-            return $content->text();
+           echo $content->text();
         }
     }
