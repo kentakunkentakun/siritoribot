649fe9f5d45f4973ac9f818a588efa17bfdb7549
kentakunkentakun <kenta.0602.2000@icloud.com>
2021-02-21 05:03:05 +0900

    g

--- index.php
+++ index.php
@@ -22,10 +22,10 @@ echo $contents;*/
 //echo textChecker('昔');
 $html = file_get_contents("https://www.weblio.jp/content/昔");
 $contentsNum = count(phpQuery::newDocument($html)->find('.kijiWrp')->find("h2"));
-$contents = phpQuery::newDocument($html)->find('.kijiWrp');
+$contents = phpQuery::newDocument($html)->find('.kijiWrp:eq(0)');
 echo $contents;
 for($i = 0; $i < $contentsNum; $i++){
-    echo $contents->find("h2:".$i.")")->text();
+    echo $contents->find("h2:eq(".$i.")")->text();
 }
 echo '777';
 //$userId = $events->getUserId();
