5a47869ff94db26d88189d43f1322e0f0c5db79d
kentakunkentakun <kenta.0602.2000@icloud.com>
2021-02-21 04:30:19 +0900

    jj

--- index.php
+++ index.php
@@ -16,13 +16,13 @@ $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATUR
 //$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 //$text = $events->getText();
 textChecker('昔');
-$userId = $events->getUserId();
+//$userId = $events->getUserId();
 /*$html = file_get_contents("https://ja.wikipedia.org/wiki/%E4%B8%89%E5%9B%BD%E5%BF%97");
 
 echo phpQuery::newDocument($html)->find(".mw-parser-output")->find('p:first')->text();*/
-foreach ($events as $event) {
+/*foreach ($events as $event) {
     // メッセージを返信
     $response = $bot->replyMessage(
         $event->getReplyToken(), new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event)  
     );
-}
+}*/
