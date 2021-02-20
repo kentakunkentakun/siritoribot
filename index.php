6c4ad55eec43907d23a7cad646190b69bfd88bb0
kentakunkentakun <kenta.0602.2000@icloud.com>
2021-02-21 04:37:31 +0900

    jj

--- index.php
+++ index.php
@@ -15,11 +15,11 @@ $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATUR
 
 //$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 //$text = $events->getText();
-$text="昔";
+/*$text="昔";
 $html = file_get_contents("https://www.weblio.jp/content/" . $text);
 $contents = phpQuery::newDocument($html)->find(".midashigo")->text();
-echo $contents;
-//textChecker('昔');
+echo $contents;*/
+echo extChecker('昔');
 //$userId = $events->getUserId();
 /*$html = file_get_contents("https://ja.wikipedia.org/wiki/%E4%B8%89%E5%9B%BD%E5%BF%97");
 
--- sukure.php
+++ sukure.php
@@ -3,8 +3,8 @@
     require_once("./phpQuery-onefile.php");
     function textChecker($text){
         $html = file_get_contents("https://www.weblio.jp/content/" . $text);
-        $contents = phpQuery::newDocument($html)->find(".midashigo")->text();
+        $contents = phpQuery::newDocument($html)->find('.kijiWrp')->find(".midashigo");
         foreach($contents as $content){
-            echo $content;
+            return $content->text();
         }
     }
