adf23c4bdd2d5b3e796589a874e8b7dcae3d3811
kentakunkentakun <kenta.0602.2000@icloud.com>
2021-02-21 20:55:29 +0900

    d

--- index.php
+++ index.php
@@ -13,20 +13,6 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '937bf98973bbd864910f4
 // LINE Messaging APIがリクエストに付与した署名を取得
 $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 echo textChecker('昔', 'む');
-$text = '昔';
-$kasira = 'む';
-$html = file_get_contents("https://www.weblio.jp/content/".$text);
-
-$contentsNum = count(phpQuery::newDocument($html)->find('.kijiWrp:eq(0)')->find("h2"));
-mb_language('Japanese');
-$html = mb_convert_encoding($html, "HTML-ENTITIES", "auto"); 
-$contents = phpQuery::newDocument($html)->find('.kijiWrp:eq(0)');
-for($i = 0; $i < $contentsNum; $i++){
-    $content = $contents->find("h2:eq(".$i.")")->text();
-    if(mb_substr($content,0,1) == $kasira){
-        echo $content;
-    }
-}
 /*$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 
 foreach ($events as $event) {
--- sukure.php
+++ sukure.php
@@ -1,13 +1,15 @@
 <?php
 
     require_once("./phpQuery-onefile.php");
+    mb_language('Japanese');
     function textChecker($text, $kasira){
         $html = file_get_contents("https://www.weblio.jp/content/".$text);
+        $html = mb_convert_encoding($html, "HTML-ENTITIES", "auto"); 
         $contentsNum = count(phpQuery::newDocument($html)->find('.kijiWrp:eq(0)')->find("h2"));
         $contents = phpQuery::newDocument($html)->find('.kijiWrp:eq(0)');
         for($i = 0; $i < $contentsNum; $i++){
             $content = $contents->find("h2:eq(".$i.")")->text();
-            if(substr($content,0,1)===$kasira){
+            if(mb_substr($content,0,1)==$kasira){
                 return $content;
             }
         }
