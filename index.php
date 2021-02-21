b0aa0a6626fa20cecf6537b58f14f4c332371d2d
kentakunkentakun <kenta.0602.2000@icloud.com>
2021-02-22 00:36:47 +0900

    g

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '937bf98973bbd864910f4
 
 // LINE Messaging APIがリクエストに付与した署名を取得
 $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-if(duplicate('kentakunkentakun', 'ふりがな')) echo '77';
+if(duplicate('kentakunkentakun', 'ふりな')) echo '77';
 else echo 'h11';
 /*$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 
