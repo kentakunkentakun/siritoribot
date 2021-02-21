34d6e7329c20e162af67db32629e0218e1bfa25a
kentakunkentakun <kenta.0602.2000@icloud.com>
2021-02-22 08:44:33 +0900

    d

--- sukure.php
+++ sukure.php
@@ -18,19 +18,13 @@
     function replyMes($userId, $gobi){
         $content;
         $checkStr;
-        while(1){
-            $num = mt_rand(0, 100);
-            $num2 = mt_rand(0,9);
-            $html = file_get_contents("https://www.weblio.jp/content_find/prefix/". $num . "/" . $gobi);
+            $html = file_get_contents("https://しりとり.net/「" . $gobi . "」からはじまる言葉/");
             $html = mb_convert_encoding($html, "HTML-ENTITIES", "auto"); 
             $content = phpQuery::newDocument($html)->find('#cntFdWrp')->find(".cntFdHead:eq(". $num2 .")")->find('.cntFdMidashi')->text();
             $checkStr = mb_substr($content, -1,1);
-            if(preg_match('/^[ぁ-を]$/',$checkStr)){
-                break;
-            }
-        }
+
         //insert($content, $content, $userId, mb_substr($content, -1,1));
-        return $content;
+        return $html;
         //return $checkStr;
     }
     function ftext($text){
