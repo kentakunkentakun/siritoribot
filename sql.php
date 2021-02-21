b5baa667c4ba08893af2ac4b3befb70f179cb2d7
kentakunkentakun <kenta.0602.2000@icloud.com>
2021-02-21 23:52:44 +0900

    f

--- sql.php
+++ sql.php
@@ -19,7 +19,7 @@ function prWord($userId){
     $stmt = $pdo->prepare("SELECT * FROM siritori WHERE userid = ? ORDER BY id DESC ");
     $stmt->execute([$userId]);
     $result = $stmt->fetch();
-    return $result;
+    return $stmt;
 }
 function insert($hurigana, $text, $userId, $gobi){
     $url = parse_url(getenv('DATABASE_URL'));
