aba23a7d9be7a7cccae5858f8f58d839e144df1b
kentakunkentakun <kenta.0602.2000@icloud.com>
2021-02-22 08:27:36 +0900

    s

--- sukure.php
+++ sukure.php
@@ -25,9 +25,9 @@
             $html = mb_convert_encoding($html, "HTML-ENTITIES", "auto"); 
             $content = phpQuery::newDocument($html)->find('#cntFdWrp')->find(".cntFdHead:eq(". $num2 .")")->find('.cntFdMidashi')->text();
             $checkStr = mb_substr($content, -1,1);
-            if(preg_match('/^[ぁ-を]$/',$checkStr)){
+            /*if(preg_match('/^[ぁ-を]$/',$checkStr)){
                 break;
-            }
+            }*/
         }
         //insert($content, $content, $userId, mb_substr($content, -1,1));
         return $content;
