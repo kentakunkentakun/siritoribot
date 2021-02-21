7afc3ee9f0ed35831d073e26e6c4dadea042e827
kentakunkentakun <kenta.0602.2000@icloud.com>
2021-02-21 20:25:27 +0900

    d

--- index.php
+++ index.php
@@ -21,8 +21,7 @@ $contentsNum = count(phpQuery::newDocument($html)->find('.kijiWrp:eq(0)')->find(
 echo $contents = phpQuery::newDocument($html)->find('.kijiWrp:eq(0)');
 for($i = 0; $i < $contentsNum; $i++){
     $content = $contents->find("h2:eq(".$i.")")->text();
-    echo $content;
-    if(substr($content,0,1)==$kasira){
+    if(substr($content,0,1) == $kasira){
         echo $content;
     }
 }
