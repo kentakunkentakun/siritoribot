7b117bb22a15913a9217d05bc7e4a2d59393bccd
kentakunkentakun <kenta.0602.2000@icloud.com>
2021-02-22 05:40:29 +0900

    t

--- index.php
+++ index.php
@@ -9,27 +9,37 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS
 
 //CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '937bf98973bbd864910f459b5fe5bd65']);
-
+if(checkPlay('kentakunkentakun')){
+    echo 'ok';
+}else{
+    echo 'hh';
+}
 // LINE Messaging APIがリクエストに付与した署名を取得
-$signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-initialize('kentakunkentakun');
-/*$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+/*$signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 
 foreach ($events as $event) {
-    {//しりとり開始
+    $text = $event->getText();
+    $userId = $event->getUserId();
 
-
-
-    }
-    {//しりとりのつづき
-        $text = $event->getText();
-        $userId = $event->getUserId();
+    if($text == 'しりとり始め'){//しりとり開始
+        if()
+        initialize($userId);
+        $response = $bot->replyMessage(
+            $event->getReplyToken(), new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ではしりとりの「り」からお願いします！')
+        );
+    }else if($text == 'しりとり終了'){//しりとりのつづき
+        reset($userId);
+        $response = $bot->replyMessage(
+            $event->getReplyToken(), new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('お疲れ様でした！')
+        );
+    }else{//しりとり終了
         $preword = prword($userId);
         $content = textChecker($text, $preword);
         $reply;
         if($content!=""){
             $ftext = mb_substr($content,0,strpos($text, '【'));
-            if(duplicate($userId,$ftext)){
+            if(duplicate($userId,$content)){
                 //正しい
 
                 insert($ftext, $content, $userId, substr($content, mb_strpos($text, '【')-1, 1));
@@ -45,9 +55,5 @@ foreach ($events as $event) {
                 $event->getReplyToken(), new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('単語が見つかりませんでした..別の単語にしてください。')  
             );
         }
-    }
-    {//しりとり終了
-
-    }
-    
+    } 
 }*/
--- sql.php
+++ sql.php
@@ -1,17 +1,5 @@
 <?php
 
-
-
-/*try {
-    
-    
-} catch (PDOException $e) {
-
-    echo $error = $e->getMessage();
-
-}*/
-
-
 function prWord($userId){
     $url = parse_url(getenv('DATABASE_URL'));
     $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
@@ -31,12 +19,12 @@ function insert($hurigana, $text, $userId, $gobi){
     var_dump($pdo -> errorInfo());
 }
 //重複確認
-function duplicate($userId, $ftext){
+function duplicate($userId, $content){
     $url = parse_url(getenv('DATABASE_URL'));
     $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
     $pdo = new PDO($dsn, $url['user'], $url['pass']);
-    $stmt = $pdo->prepare("SELECT * FROM siritori WHERE userId = ? AND hurigana = ? ORDER BY id DESC ");
-    $stmt->execute([$userId, $ftext]);
+    $stmt = $pdo->prepare("SELECT * FROM siritori WHERE userId = ? AND word = ? ORDER BY id DESC ");
+    $stmt->execute([$userId, $content]);
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     if(empty($result)){
         return true;
@@ -44,14 +32,31 @@ function duplicate($userId, $ftext){
         return false;
     }
 }
-
 function initialize($userId){
+    $url = parse_url(getenv('DATABASE_URL'));
+    $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+    $pdo = new PDO($dsn, $url['user'], $url['pass']);
+    $reset = $pdo->prepare("INSERT INTO siritori (hurigana, word, userid, gobi) VALUES (?, ?, ?, ?)");
+    $reset->execute(['しりとり', 'しりとり', $userId, 'り']);
+}
+function reset($userId){
     $url = parse_url(getenv('DATABASE_URL'));
     $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
     $pdo = new PDO($dsn, $url['user'], $url['pass']);
     $stmt = $pdo->prepare("DELETE FROM siritori WHERE userId = ?");
     $stmt->execute([$userId]);
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    $reset = $pdo->prepare("INSERT INTO siritori (hurigana, word, userid, gobi) VALUES (?, ?, ?, ?)");
-    $reset->execute(['しりとり', 'しりとり', $userId, 'り']);
+}
+function checkPlay($userId){
+    $url = parse_url(getenv('DATABASE_URL'));
+    $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+    $pdo = new PDO($dsn, $url['user'], $url['pass']);
+    $stmt = $pdo->prepare("SELECT * FROM playing WHERE userId = ?");
+    $stmt->execute([$userId]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    if(!empty($result)){
+        return true;
+    }else{
+        return false;
+    }
 }
